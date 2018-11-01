1ffa1a3869ad5aefc2e241f0fcbf69b115e1e279
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 20:57:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,6 +16,9 @@ class Pokemon
   
   def self.find(id, db)
     pokemon_parts = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    new_id = pokemon_parts[0]
+    new_name = pokemon_parts[1]
+    new_type = pokemon_parts[2]
     
   end
   
